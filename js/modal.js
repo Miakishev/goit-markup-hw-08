b18922a8306984f3d5c1252b221a2c64f9659ec3
Miakishev <makisevm12@gmail.com>
2022-07-16 16:04:41 +0300

    done

--- js/modal.js
+++ js/modal.js
@@ -8,7 +8,29 @@
    refs.openModalBtn.addEventListener('click', toggleModal);
    refs.closeModalBtn.addEventListener('click', toggleModal);
 
+
+
    function toggleModal() {
       refs.modal.classList.toggle('is-hidden');
    }
+
 })();
+
+(() => {
+   const refs2 = {
+      openModalBtn2: document.querySelector('[data-modal-open2]'),
+      closeModalBtn2: document.querySelector('[data-modal-close2]'),
+      modal2: document.querySelector('[data-modal2]'),
+   };
+
+   refs2.openModalBtn2.addEventListener('click', toggleModal);
+   refs2.closeModalBtn2.addEventListener('click', toggleModal);
+
+
+
+   function toggleModal() {
+      refs2.modal2.classList.toggle('is-hidden');
+   }
+
+})();
+
